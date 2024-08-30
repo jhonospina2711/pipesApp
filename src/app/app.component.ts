aff7b66f935fcaef57da53fc6b2c31c22f76a848
Jhon Ospina <jhonospina2711@gmail.com>
2024-08-30 00:17:25 -0500

    Seccion 11 - Implementación de PrimeNg, Menu, MenuBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {PrimeNGConfig } from 'primeng/api'
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule
+],
   providers: [
     provideClientHydration()
   ],
